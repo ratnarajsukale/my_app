0e2a3a5e01fd9d392c4cb62200cd0263083a3664
Ratnaraj Sukale <rvsukale@gmail.com>
2021-05-06 13:50:57 +0530

    add account activation

--- Gemfile
+++ Gemfile
@@ -59,3 +59,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# For opening email in development  
+gem "letter_opener", :group => :development
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,22 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      reset_session
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      log_in user
-      redirect_to forwarding_url || user
-      
-    # Log the user in and redirect to the user's show page.
+    if user && user.authenticate(params[:session][:password])
+      if user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user) 
+        log_in user
+        redirect_to forwarding_url || user 
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link." 
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      flash.now[:denger] = "Invalid Email/Password combination "
-      # Create an error message.
-      render "new"
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
 

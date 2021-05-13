0b8734ba2f6c051491c2d0badb7e6b378b000ce7
Ratnaraj Sukale <rvsukale@gmail.com>
2021-05-13 16:29:11 +0530

    Removed whitespace

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
         log_in user
         redirect_to forwarding_url || user 
       else
-        message = "Account not activated. "
+        message = "Account not activated."
         message += "Check your email for the activation link." 
         flash[:warning] = message
         redirect_to root_url
